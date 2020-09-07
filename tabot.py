6d04389b1d14e61486ccf572789023bbdd8db709
Matheus Almeida <matheush20@outlook.com>
2020-09-06 21:22:09 -0300

    Crie função autopick. corrigi bug que personagem ficava atacando sem ter ninguem selecionado

--- tabot.py
+++ tabot.py
@@ -1,6 +1,15 @@
 from tools import Talisman, Ataque
-from pyautogui import press
-from time import sleep
+from pyautogui import press, moveTo, click
+from time import sleep, time
+from math import trunc
+
+
+def autopick(Pos=[515, 418], Pulo=55):
+    for x in range(417, 623, Pulo):
+        for y in range(311, 485, Pulo):
+            click(x=x, y=y, button='right')
+    moveTo(x=Pos[0], y=Pos[1])
+    click()
 
 
 def config(clase_personagem, Vida, Mana=None, BotaoCura=None, BotaoMana=None, FayV=3200):
@@ -31,9 +40,9 @@ ataque = Ataque('s')
 sleep(3)
 
 while True:
-    # selecionando a classe
-    config('s', Vida=100, BotaoCura=0)
 
+    # selecionando a classe
+    config('f', Vida=1300, Mana=1300, BotaoMana=9, BotaoCura=6, FayV=4000)
 
     # selecionando inimigo
     while True:
@@ -42,10 +51,20 @@ while True:
         if EnimigoSelect >= 1:
             break
 
+    TempoInicio = time()
     # atacando
     while True:
-        ataque.Atacar(3)  # selecione o ataque
+        ataque.Atacar(2)  # selecione o ataque
         VidaEnimigo = talisman.get_info('ve')
+
+        # verificando se o bot esta muito tempo parado
+        TempoFinal = time()
+        Minutos = trunc(TempoFinal - TempoInicio)
+
+        if Minutos > 60:
+            break
 
         if VidaEnimigo == 0:
             break
+
+    autopick()
