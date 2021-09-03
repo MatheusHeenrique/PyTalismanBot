b39b8f44375a9c2f2a39fcb8bc499714d016c661
Matheus Almeida <matheush20@outlook.com>
2021-09-03 15:08:07 -0300

    Atualizei as variaveis do ler_memoria

--- tools/ler_memoria.py
+++ tools/ler_memoria.py
@@ -8,13 +8,11 @@ class LerTalisman:
         self.programa.open()
 
         # ponteiros
-        self.ponteiro_vida = self.programa.get_pointer(0x00400000 + 0x00BFB980, offsets=[0x320]) # check
-        self.ponteiro_mana = self.programa.get_pointer(0x00400000 + 0x00BFB980, offsets=[0x324]) # check
-        self.ponteiro_estamina = self.programa.get_pointer(0x00400000 + 0x00BFB980, offsets=[0x344])  # check
-        self.ponteiro_selecionar_inimigo = self.programa.get_pointer(0x00400000 + 0x00EA7CF4, offsets=[0xB4, 0x174, 0x48, 0x28, 0x9C, 0x18, 0x784]) # check
-        self.ponteiro_vida_inimigo = self.programa.get_pointer(0x00400000 + 0x00EA7B78,
-                                                               offsets=[0x18, 0x59C, 0x0, 0xC, 0x1F4, 0x50, 0x28, 0x224,
-                                                                    0x50, 0x28, 0xDB0]) # check
+        self.ponteiro_vida = self.programa.get_pointer(0x00400000 + 0x00C0E980, offsets=[0x7A0, 0x64, 0x0, 0xE4, 0x10, 0x218, 0x758]) # check
+        self.ponteiro_mana = self.programa.get_pointer(0x00400000 + 0x00D325F0, offsets=[0x64, 0x0, 0x4C, 0x4, 0x7A4, 0x1D8, 0x75C]) # check
+        self.ponteiro_estamina = self.programa.get_pointer(0x00400000 + 0x00C0E980, offsets=[0x8, 0x40, 0x4, 0x4, 0x4, 0xC, 0x3DC])  # check
+        self.ponteiro_selecionar_inimigo = self.programa.get_pointer(0x00400000 + 0x00EAD9A8, offsets=[0xD0, 0x2DC, 0x24, 0x10, 0x28, 0x2C, 0xA48]) # check
+        self.ponteiro_vida_inimigo = self.programa.get_pointer(0x00400000 + 0x00C0E97C, offsets=[0x4, 0xB0, 0x0, 0x8, 0x430, 0x18, 0x0, 0x48, 0x28, 0x80, 0x8F8]) # check
 
     def catar_info(self, valor):
         if valor == 'v':  # verifica a vida
